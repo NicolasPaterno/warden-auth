b2032a2ff2cc4101564ef860d5916689db2a6c28
NicolasPaterno <nicolasps.contato@gmail.com>
2026-06-10 01:52:08 -0300

    feat(db): add users migration, queries and sqlc config

---
+++ db/queries/users.sql
@@ -0,0 +1,7 @@
+-- name: GetUserByEmail :one
+SELECT * FROM users WHERE email = $1;
+
+-- name: CreateUser :one
+INSERT INTO users (id, email, password_hash, created_at)
+VALUES ($1, $2, $3, $4)
+    RETURNING *;
